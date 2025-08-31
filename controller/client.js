fe40d8f4a86cd7a1ed0310ffee8deb4cd6fc94e8
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2025-08-31 16:17:52 +0530

    fix bugs

--- controller/client.js
+++ controller/client.js
@@ -1,15 +1,4 @@
-const { setClientAuth } = require("../authentication/auth");
 const client = require("../model/client");
-
-async function handlegetHome(req,res){
-    if(req.client) {
-        console.log("payload : ",req.client)
-      const loginclient =await client.findOne({email:req.client.email})
-      res.render('home',{
-          loginClient:loginclient
-        })
-    }
-}
 
 async function handlesignup(req,res){
     const {name,password,email,field} = req.body;
@@ -30,17 +19,4 @@ async function handlegetallData(req,res){
     return res.send({clients:clients})
 }
 
-async function handleLogin(req,res){
-const {email, password} = req.body
-const foundClient =  await client.findOne({email:email, password:password})
-if(!foundClient) return res.render('login',{
-    err:'email or password not correct!'
-})
-const token = setClientAuth(foundClient)
-res.cookie('client',token)
-
-    return res.redirect('/')
-}
-
-
-module.exports = {handlegetHome,handlesignup,handleLogin,handlegetallData}
+module.exports = {handlesignup,handlegetallData}
--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const client = require('./model/client');
-const {handlegetHome,handlesignup,handleLogin,handlegetallData} = require('./controller/client');
+const {handlesignup,handlegetallData} = require('./controller/client');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const { authCheck, authorizationCheck } = require('./middleware/authCheck');
 
 const app = express();
 const PORT = 2000;
@@ -22,13 +21,9 @@ app.use(cors({
    origin:'http://localhost:5173',
    credentials:true
 }));
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname,'views'))
 
-app.get('/',handlegetHome)
 app.post('/',handlesignup)
 app.get('/clients',handlegetallData)
-app.post('/user',handleLogin)
 
 app.listen(PORT,()=>{
     console.log('server started !')
