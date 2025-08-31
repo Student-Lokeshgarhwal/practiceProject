99b726c9da17bbe63af400ef615586080adf3ac0
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2025-08-31 19:57:42 +0530

    add mongodb

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = process.env.PORT || 2000;
-const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/PP";
+const DB_URL = process.env.DB_URL;
 
 mongoose.connect(DB_URL)
 .then((res)=>console.log('DB connted !'))
